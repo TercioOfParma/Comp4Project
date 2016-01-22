4dc9de2faa485879e45e9d673dffebb9c981e036
Elliot Lake <gigaraptorama@gmail.com>
2016-01-22 11:39:53 +0000

    Same as prior for draw.c

--- src/draw.c
+++ src/draw.c
@@ -18,40 +18,42 @@
 
 */
 
-void drawMenuElements(buttonData **buttons,int size, SDL_Renderer *render)
+void drawMenuElements( buttonData **buttons , int size , SDL_Renderer *render )
 {
-	fprintf(stderr, "Drawing buttons....\n");
+	fprintf( stderr , "Drawing buttons....\n" );
 	int i;
-	for(i =0; i < size; i++)
+	for( i = 0 ; i < size ; i++ )
 	{
-		SDL_RenderCopy(render, buttons[i]->display,NULL,  &(buttons[i]->dimensions));
+		SDL_RenderCopy( render , buttons[i]->display , NULL ,  &(buttons[i]->dimensions) );
 	}
 
 }
 /*
 	textData *renderText(TTF_Font *font, SDL_Renderer *render, const char *stringToTexture):
 	Creates a textData structure, and gives it a texture from the instructed text
-
+	Mallocs some stuff, also deals with some memory allocation to GPU by library
 */
-textData *renderText(TTF_Font *font, SDL_Renderer *render, const char *stringToTexture ,int *success)
+textData *renderText( TTF_Font *font , SDL_Renderer *render , const char *stringToTexture , int *success )
 {
-	fprintf(stderr, "Rendering text....\n");
-	textData *temp = malloc(sizeof(textData));
+	fprintf( stderr , "Rendering text....\n" );
+	textData *temp = malloc( sizeof( textData ) );
 	SDL_Surface *tempSurf;
 	SDL_Texture *tempTex;
-	char renderableText [MAX_TEXT_OUTPUT];
+	char renderableText [MAX_TEXT_OUTPUT];//what will be displayed
+	
 	snprintf(renderableText, MAX_TEXT_OUTPUT, "%s", stringToTexture);//no sprintf overflows here 
-	tempSurf = TTF_RenderText_Solid(font, renderableText, DEFAULT_TEXT);
-	if(!tempSurf)
+	tempSurf = TTF_RenderText_Solid( font , renderableText , DEFAULT_TEXT );
+	
+	if( !tempSurf )
 	{
-		fprintf(stderr, "TTF_RenderText_Solid has failed : %s \n", SDL_GetError());
+		fprintf( stderr , "TTF_RenderText_Solid has failed : %s \n" ,  SDL_GetError() );
 		*success = FAIL;
 		return NULL;
 	}
-	tempTex = SDL_CreateTextureFromSurface(render, tempSurf);
-	if(!tempTex)
+	tempTex = SDL_CreateTextureFromSurface( render , tempSurf );
+	if( !tempTex )
 	{
-		fprintf(stderr, "SDL_CreateTextureFromSurface has failed : %s \n", SDL_GetError());
+		fprintf( stderr , "SDL_CreateTextureFromSurface has failed : %s \n", SDL_GetError() );
 		*success = FAIL;
 		return NULL;
 	
@@ -59,7 +61,7 @@ textData *renderText(TTF_Font *font, SDL_Renderer *render, const char *stringToT
 	temp->display = tempTex;
 	temp->dimensions.w = tempSurf->w;
 	temp->dimensions.h = tempSurf->h;
-	SDL_FreeSurface(tempSurf);
+	SDL_FreeSurface( tempSurf );
 	return temp; //cannot simply destroy since then the address would then be hanging
 }
 /*
@@ -67,72 +69,71 @@ textData *renderText(TTF_Font *font, SDL_Renderer *render, const char *stringToT
 	draws a textData structure, (effectively a glorified wrapper function at this point
 
 */
-void drawText(textData *toDraw, SDL_Renderer *render)
+void drawText( textData *toDraw , SDL_Renderer *render )
 {
-	fprintf(stderr, "Drawing text....\n");
-	SDL_RenderCopy(render, toDraw->display, NULL, &(toDraw->dimensions));
-
+	fprintf( stderr, "Drawing text....\n" );
+	SDL_RenderCopy( render , toDraw->display , NULL , &(toDraw->dimensions) );
 }
 /*
 	void drawMenuElementsText(buttonDataText **buttons, int size, SDL_Renderer *render):
 	draws a buttonData structure and associated textData
 
 */
-void drawMenuElementsText(buttonDataText **buttons, int size, SDL_Renderer *render)
+void drawMenuElementsText( buttonDataText **buttons , int size , SDL_Renderer *render )
 {
-	fprintf(stderr, "Drawing buttons with text....\n");
+	fprintf( stderr , "Drawing buttons with text....\n" );
 	int i;
-	for(i = 0; i < size; i++)
+	for( i = 0 ; i < size ; i++ )
 	{
-		SDL_RenderCopy(render, buttons[i]->display,NULL,  &(buttons[i]->dimensions));
-		drawText(buttons[i]->details, render);//need to give it the textData address
+		SDL_RenderCopy( render , buttons[i]->display , NULL ,  &(buttons[i]->dimensions) );
+		drawText( buttons[i]->details , render );//need to give it the textData address
 
 	}
 }
 /*
 	void drawQuote(quoteData **quotes, int quoteNo, SDL_Renderer *render, TTF_Font *font):
 	Draws a quote an its analysis in a set place, will be done at the end of a turn
-
+	Deals with memory!
 */
-void drawQuote(quoteData **quotes, int quoteNo, SDL_Renderer *render, TTF_Font *font)
+void drawQuote( quoteData **quotes , int quoteNo , SDL_Renderer *render , TTF_Font *font )
 {
-	fprintf(stderr, "Drawing quote....\n");
+	fprintf( stderr , "Drawing quote....\n" );
 	int wasSuccess = SUCCESS;
-	quotes[quoteNo]->display[QUOTE_POS] = renderText(font, render, (quotes[quoteNo]->quote), &wasSuccess);
-	quotes[quoteNo]->display[ANALYSIS_POS] = renderText(font, render, (quotes[quoteNo]->analysis), &wasSuccess);
-	if(wasSuccess == FAIL)
+	quotes[ quoteNo ]->display[ QUOTE_POS ] = renderText( font , render , quotes[quoteNo]->quote , &wasSuccess );
+	quotes[ quoteNo ]->display[ ANALYSIS_POS ] = renderText( font , render , quotes[ quoteNo ]->analysis , &wasSuccess );
+	if( wasSuccess == FAIL )
 	{
-		fprintf(stderr, "One of the quote components has failed, please check the file and your computer\n");//NON CRITICAL FAILURE
+		fprintf( stderr , "One of the quote components has failed, please check the file and your computer\n" );//NON CRITICAL FAILURE
 		return;
-	
 	}
-	quotes[quoteNo]->display[QUOTE_POS]->dimensions.x = QUOTATION_XPOS;
-	quotes[quoteNo]->display[QUOTE_POS]->dimensions.y = QUOTATION_YPOS;
-	quotes[quoteNo]->display[ANALYSIS_POS]->dimensions.x = QUOTATION_XPOS;
-	quotes[quoteNo]->display[ANALYSIS_POS]->dimensions.y = QUOTATION_YPOS + 100;
-	drawText(quotes[quoteNo]->display[QUOTE_POS], render);
-	drawText(quotes[quoteNo]->display[ANALYSIS_POS], render);
+	quotes[ quoteNo ]->display[ QUOTE_POS ]->dimensions.x = QUOTATION_XPOS ;
+	quotes[ quoteNo ]->display[ QUOTE_POS ]->dimensions.y = QUOTATION_YPOS ;
+	quotes[ quoteNo ]->display[ ANALYSIS_POS ]->dimensions.x = QUOTATION_XPOS ;
+	quotes[ quoteNo ]->display[ ANALYSIS_POS ]->dimensions.y = QUOTATION_YPOS + 100 ;
+	drawText(quotes[ quoteNo ]->display[ QUOTE_POS ], render );
+	drawText(quotes[ quoteNo ]->display[ ANALYSIS_POS ], render );
 }
 /*
 	void drawQuestion(questionData **questions, int quoteNo, SDL_Renderer *render):
 	Draws a quote an its analysis in a set place, will be done at the end of a turn
 
 */
-void drawQuestion(questionData **questions, int questionNo, SDL_Renderer *render, TTF_Font *font)
+void drawQuestion( questionData **questions , int questionNo , SDL_Renderer *render , TTF_Font *font )
 {
-	fprintf(stderr, "Drawing question and answers....\n");
+	fprintf( stderr , "Drawing question and answers....\n" );
 	int wasSuccess = SUCCESS;
 	int i;
-	questions[questionNo]->display[QUESTION_POS] = renderText(font, render, questions[questionNo]->question, &wasSuccess);
-	questions[questionNo]->display[QUESTION_POS]->dimensions.x = QUESTION_XPOS;
-	questions[questionNo]->display[QUESTION_POS]->dimensions.y = QUESTION_YPOS;
-	drawText(questions[questionNo]->display[QUESTION_POS], render);
-	for(i = 0; i < NO_ANSWERS; i++)
+	
+	questions[ questionNo ]->display[ QUESTION_POS ] = renderText( font , render , questions[ questionNo ]->question , &wasSuccess );
+	questions[ questionNo ]->display[ QUESTION_POS ]->dimensions.x = QUESTION_XPOS ;
+	questions[ questionNo ]->display[ QUESTION_POS ]->dimensions.y = QUESTION_YPOS ;
+	drawText( questions[ questionNo ]->display[ QUESTION_POS ] , render );
+	for(i = 1;  i <= NO_ANSWERS;  i++)//this may not work, check when get home!
 	{
-		questions[questionNo]->display[i + 1]= renderText(font, render, questions[questionNo]->answers[i], &wasSuccess);
-		questions[questionNo]->display[i + 1]->dimensions.x = QUESTION_XPOS;
-		questions[questionNo]->display[i + 1]->dimensions.y = QUESTION_YPOS + (50 * (i + 1));
-		drawText(questions[questionNo]->display[i + 1], render);
+		questions[ questionNo ]->display[i]= renderText(font, render, questions[questionNo]->answers[i - 1], &wasSuccess);
+		questions[ questionNo ]->display[i]->dimensions.x = QUESTION_XPOS;
+		questions[ questionNo ]->display[i]->dimensions.y = QUESTION_YPOS + (50 * i) ;
+		drawText( questions[ questionNo ]->display[i] , render );
 	}
 
 }
@@ -141,13 +142,14 @@ void drawQuestion(questionData **questions, int questionNo, SDL_Renderer *render
 	Draws a map tile from the base texture that has all the textures
 
 */
-void drawTerrain(tileData **toDraw, int size, SDL_Renderer *render, SDL_Texture *tileMap)
+void drawTerrain( tileData **toDraw , int size , SDL_Renderer *render , SDL_Texture *tileMap )
 {
-	fprintf(stderr, "Drawing terrain....\n");
+	fprintf( stderr , "Drawing terrain....\n" );
 	int i;
-	for(i = 0; i < size; i++)
+	for( i = 0 ; i < size ; i++)
 	{
-		SDL_RenderCopyEx(render, tileMap, &(toDraw[i]->spriteDimensions), &(toDraw[i]->dimensions), toDraw[i]->angle, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx( render , tileMap , &(toDraw[i]->spriteDimensions) , &(toDraw[i]->dimensions) ,
+		toDraw[i]->angle , NULL, SDL_FLIP_NONE );
 	}
 }
 /*
@@ -155,15 +157,15 @@ void drawTerrain(tileData **toDraw, int size, SDL_Renderer *render, SDL_Texture 
 	Draws a unit tile from the base texture that has all the textures
 
 */
-void drawUnits(unitData **toDraw, int size, SDL_Renderer *render, SDL_Texture *tileMap)
+void drawUnits( unitData **toDraw , int size , SDL_Renderer *render , SDL_Texture *tileMap )
 {
-	fprintf(stderr, "Drawing units....\n");
+	fprintf( stderr , "Drawing units....\n" );
 	int i;
-	for(i = 1; i <= size; i++)
+	for( i = 1 ; i <= size; i++ )
 	{
-		if(toDraw[i]->alive = TRUE)
+		if( toDraw[i]->alive = TRUE )
 		{
-			SDL_RenderCopy(render, tileMap, &(toDraw[i]->spriteDimensions), &(toDraw[i]->dimensions));
+			SDL_RenderCopy( render , tileMap , &(toDraw[i]->spriteDimensions) , &(toDraw[i]->dimensions) );
 		}
 	}
 }
