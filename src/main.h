140946d8a37027ddc67696ebede0b788310dfcf9
Elliot Lake <gigaraptorama@gmail.com>
2016-02-08 15:08:08 +0000

    Spaced out main.h

--- src/main.h
+++ src/main.h
@@ -9,17 +9,17 @@
 
 
 //----------------------------------- HEADER FILES AND C PREPROCESSOR (MINUS #define)------------------------
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-#include <jansson/jansson.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <time.h>
+#include < SDL.h >
+#include < SDL_image.h >
+#include < SDL_ttf.h >
+#include < SDL_mixer.h >
+#include < jansson/jansson.h >
+#include < stdio.h >
+#include < stdlib.h >
+#include < math.h >
+#include < fcntl.h >
+#include < unistd.h >
+#include < time.h >
 //----------------------------------- PROGRAM CONSTANTS --------------------------------------
 #define SIZE_OF_MODIFIERS 6
 #define NO_SIDES 2
@@ -145,7 +145,7 @@ typedef struct
 	char *name;
 	char *description;
 	//in simulation modifiers
-	char modifiers [SIZE_OF_MODIFIERS] ;
+	char modifiers [ SIZE_OF_MODIFIERS ] ;
 	char alive;
 	int moved;
 	int shot;
@@ -234,7 +234,7 @@ typedef struct
 {
 	char *quote;
 	char *analysis;
-	textData *display [TEXT_DATA_QUOTE];
+	textData *display [ TEXT_DATA_QUOTE ];
 
 }quoteData;
 /*
@@ -255,9 +255,9 @@ typedef struct
 typedef struct
 {
 	char *question;
-	char *answers[NO_ANSWERS];
+	char *answers[ NO_ANSWERS ];
 	int answerNo;
-	textData *display [TEXT_DATA_QUESTION];
+	textData *display [ TEXT_DATA_QUESTION ];
 
 }questionData;
 /*
@@ -293,7 +293,7 @@ typedef struct
 	//subordinate scenario componenets
 	tileData **tiles;
 	quoteListData *quoteList;
-	sideData *sides[NO_SIDES];
+	sideData *sides[ NO_SIDES ];
 	//map information
 	int mapID;
 	char *title;
@@ -315,75 +315,134 @@ typedef struct
 //----------------------------------- FUNCTION PROTOTYPES ------------------------------------
 //-----------INITIALISATION-----------
 //loading libraries and the files themselves
-char *loadTextFile(const char *filename, int *success);// DONE
-int getFileSize(FILE *sizeToGet, int *success);// DONE
-optionsData initOptions(char *fileContents, int *success);// DONE
-SDL_Window *initSDL(optionsData *opt, int *success);// DONE
-SDL_Renderer *createRenderer(SDL_Window *screen, int *success);// DONE
-Mix_Music *loadMusic(const char *filename, int *success);// DONE
-Mix_Chunk *loadEffect(const char *filename, int *success);// DONE
-TTF_Font *loadFont(optionsData *opt, int *success);// DONE
-SDL_Texture *loadImage(const char *filename, SDL_Renderer *render, SDL_Rect *dimen, int *success);// DONE
+char *loadTextFile( const char *filename , int *success );// DONE
+
+int getFileSize( FILE *sizeToGet , int *success );// DONE
+
+optionsData initOptions( char *fileContents , int *success );// DONE
+
+SDL_Window *initSDL( optionsData *opt , int *success );// DONE
+
+SDL_Renderer *createRenderer( SDL_Window *screen , int *success );// DONE
+
+Mix_Music *loadMusic( const char *filename , int *success );// DONE
+
+Mix_Chunk *loadEffect( const char *filename , int *success );// DONE
+
+TTF_Font *loadFont( optionsData *opt , int *success );// DONE
+
+SDL_Texture *loadImage( const char *filename , SDL_Renderer *render , SDL_Rect *dimen , int *success );// DONE
+
 //runtime objects
-buttonData *loadButton(SDL_Texture *display, SDL_Rect *posAndSize, int type, int *success);// DONE
+buttonData *loadButton( SDL_Texture *display , SDL_Rect *posAndSize , int type , int *success );// DONE
+
 //there was createTextData here, but i felt it unneccessary, Can always roll back changes need be
-buttonDataText *loadButtonText(SDL_Texture *display, SDL_Rect *posAndSize, SDL_Renderer *render, const char *initialData, TTF_Font *font, int type, int *success);//DONE
-levelData *loadLevelData(SDL_Renderer *render, int *success);//DONE
-char *mapLevelIDToMapPath(int id, int *success);//DONE
-char *miscIDToFilePath(int ID, char *path);//DONE
-mapData *loadMapData(char *levelDataFile ,int mapNo, SDL_Renderer *render,int *success);//DONE
-sideData *loadSideData(char *filename,int sideNumber, int *success);//DONE
-unitData **loadUnitData(char *sideUnitDataFilePath, char *unitDescriptorDataFilePath, int *success);//DONE
-tileData **loadTileData(char *tileFile, int *success);//DONE
-quoteListData *loadQuoteListData(char *filename, int *success);//DONE
-quoteData **loadQuotes(char *filename, int *success);//DONE
-activityData *loadActivity(char *filename, int *success);//DONE
-questionData **loadQuestions(char *filename, int *success);//DONE
-unitData *loadUnit(char *unitFile, int ID, int *success);//DONE
+buttonDataText *loadButtonText( SDL_Texture *display , SDL_Rect *posAndSize , SDL_Renderer *render , const char *initialData , TTF_Font *font , int type , int *success );//DONE
+
+levelData *loadLevelData( SDL_Renderer *render , int *success );//DONE
+
+char *mapLevelIDToMapPath( int id , int *success );//DONE
+
+char *miscIDToFilePath( int ID , char *path );//DONE
+
+mapData *loadMapData( char *levelDataFile , int mapNo , SDL_Renderer *render , int *success );//DONE
+
+sideData *loadSideData( char *filename , int sideNumber , int *success );//DONE
+
+unitData **loadUnitData( char *sideUnitDataFilePath , char *unitDescriptorDataFilePath , int *success );//DONE
+
+tileData **loadTileData( char *tileFile , int *success );//DONE
+
+quoteListData *loadQuoteListData( char *filename , int *success );//DONE
+
+quoteData **loadQuotes( char *filename , int *success );//DONE
+
+activityData *loadActivity( char *filename , int *success );//DONE
+
+questionData **loadQuestions( char *filename , int *success );//DONE
+
+unitData *loadUnit( char *unitFile , int ID , int *success );//DONE
+
 //----------DEINITIALISATION-----------
-void endSDL(SDL_Renderer *render, SDL_Window *screen, TTF_Font *font);//DONE
-void endUnitDataArray(unitData **units, int size);//DONE
-void endSideData(sideData *side);//DONE
-void endTileData(tileData **tiles, int size);//DONE
-void endQuoteListData(quoteListData *quotes);//DONE
-void endQuotes(quoteData **quotes, int size);//DONE
-void endActivity(activityData *activity);//DONE
-void endQuestions(questionData **questions, int size);//DONE
-void endLevel(levelData *level);//DONE
+void endSDL( SDL_Renderer *render , SDL_Window *screen , TTF_Font *font );//DONE
+
+void endUnitDataArray( unitData **units , int size );//DONE
+
+void endSideData( sideData *side );//DONE
+
+void endTileData( tileData **tiles , int size );//DONE
+
+void endQuoteListData( quoteListData *quotes );//DONE
+
+void endQuotes( quoteData **quotes , int size );//DONE
+
+void endActivity( activityData *activity );//DONE
+
+void endQuestions( questionData **questions , int size );//DONE
+
+void endLevel( levelData *level );//DONE
+
 
 //----------DRAWING AND GRAPHICAL-----
-void drawTerrain(tileData **toDraw, int size, SDL_Renderer *render, SDL_Texture *tileMap);//DONE
-void drawUnits(unitData **toDraw, int size,  SDL_Renderer *render, SDL_Texture *tileMap);//DONE
-void drawMenuElements(buttonData **buttons,int size, SDL_Renderer *render);// DONE
-void drawMenuElementsText(buttonDataText **buttons, int size, SDL_Renderer *render);//DONE
-textData *renderText(TTF_Font *font, SDL_Renderer *render, const char *stringToTexture,int *success);// DONE
-void drawText(textData *toDraw,SDL_Renderer *render);// DONE
-void drawQuote(quoteData **quotes, int quoteNo, SDL_Renderer *render, TTF_Font *font);//DONE
-void drawQuestion(questionData **questions, int questionNo, SDL_Renderer *render, TTF_Font *font);//DONE
+void drawTerrain( tileData **toDraw , int size , SDL_Renderer *render , SDL_Texture *tileMap );//DONE
+
+void drawUnits( unitData **toDraw , int size ,  SDL_Renderer *render , SDL_Texture *tileMap );//DONE
+
+void drawMenuElements( buttonData **buttons , int size , SDL_Renderer *render );// DONE
+
+void drawMenuElementsText( buttonDataText **buttons , int size , SDL_Renderer *render );//DONE
+
+textData *renderText( TTF_Font *font , SDL_Renderer *render , const char *stringToTexture , int *success );// DONE
+
+void drawText( textData *toDraw , SDL_Renderer *render );// DONE
+
+void drawQuote( quoteData **quotes , int quoteNo , SDL_Renderer *render , TTF_Font *font );//DONE
+
+void drawQuestion( questionData **questions , int questionNo , SDL_Renderer *render , TTF_Font *font );//DONE
+
 
 
 //----------INPUT---------------------
-int checkButtonClicked(SDL_Rect *mouseDimensions, buttonData *button);//DONE
-int checkButtonTextClicked(SDL_Rect *mouseDimensions, buttonDataText *button);//DONE
-int handleMouseButtonMainMenu(buttonData **buttons, int size, SDL_Renderer *render, SDL_Event *events);//DONE
-int handleMouseButtonSelectionMenu(buttonDataText **buttonsText, int size, SDL_Renderer *render, SDL_Event *events);//DONE
-int handleKeyboardSimulation(SDL_Event *keyboardInput, unitData **units);//DONE
-int handleMapClicked(sideData *applicableUnits, sideData *otherSide, tileData **tiles,  buttonData **endTurn, SDL_Event *events);//DONE
-int checkQuestionClicked(SDL_Rect *mouseDimensions, questionData *question, int answerNo);//DONE
-int checkUnitClicked(SDL_Rect *mouseDimensions, unitData *unit);//DONE
-int checkTileClicked(SDL_Rect *mouseDimensions, tileData *tile);//DONE
+
+int checkButtonClicked( SDL_Rect *mouseDimensions , buttonData *button );//DONE
+
+int checkButtonTextClicked( SDL_Rect *mouseDimensions , buttonDataText *button );//DONE
+
+int handleMouseButtonMainMenu( buttonData **buttons , int size , SDL_Renderer *render , SDL_Event *events );//DONE
+
+int handleMouseButtonSelectionMenu( buttonDataText **buttonsText , int size , SDL_Renderer *render , SDL_Event *events );//DONE
+
+int handleKeyboardSimulation( SDL_Event *keyboardInput , unitData **units );//DONE
+
+int handleMapClicked( sideData *applicableUnits , sideData *otherSide , tileData **tiles ,  buttonData **endTurn , SDL_Event *events );//DONE
+
+int checkQuestionClicked( SDL_Rect *mouseDimensions , questionData *question , int answerNo );//DONE
+
+int checkUnitClicked( SDL_Rect *mouseDimensions , unitData *unit );//DONE
+
+int checkTileClicked( SDL_Rect *mouseDimensions , tileData *tile );//DONE
 
 //---------SIMULATION----------------
-int aStarWithTerrain(sideData *applicableUnits, int unitNo, tileData **tiles, int xPos, int yPos, int size);//DONE
-void moveUnit(sideData *applicableUnits, tileData **tiles, int xPos, int yPos, int givenUnit);//DONE
-int shootUnit(sideData *shootingSideUnits, int shootingSideNo, sideData *recievingSideUnits, int recievingSideNo, tileData **tiles);//DONE
-void resolveShooting(sideData *shootingSideUnits, int shootingSideNo, sideData *recievingSideUnits, int recievingSideNo,int inflictedCasualties, int recievedCasualties);//DONE
-void displaySimulationResults(sideData *sideOne, sideData *sideTwo, tileData **map, TTF_Font *font, SDL_Renderer *render);//DONE
-int findDistance(tileData *tileOne, tileData *tileTwo);//DONE
-void simulationMain(mapData *map, SDL_Renderer *render, TTF_Font *font, buttonData **nextTurn, SDL_Event *events, int *success);
+
+int aStarWithTerrain( sideData *applicableUnits , int unitNo , tileData **tiles , int xPos , int yPos , int size );//DONE
+
+void moveUnit( sideData *applicableUnits , tileData **tiles , int xPos , int yPos , int givenUnit );//DONE
+
+int shootUnit( sideData *shootingSideUnits , int shootingSideNo , sideData *recievingSideUnits , int recievingSideNo , 
+tileData **tiles );//DONE
+
+void resolveShooting( sideData *shootingSideUnits , int shootingSideNo , sideData *recievingSideUnits , int recievingSideNo , int inflictedCasualties , int recievedCasualties );//DONE
+
+void displaySimulationResults( sideData *sideOne , sideData *sideTwo , tileData **map , TTF_Font *font , SDL_Renderer *render );//DONE
+
+int findDistance( tileData *tileOne , tileData *tileTwo );//DONE
+
+void simulationMain( mapData *map , SDL_Renderer *render , TTF_Font *font , buttonData **nextTurn , SDL_Event *events , int *success );
 
 //---------ACTIVITY------------------
-int startQuiz(activityData *quiz, SDL_Renderer *render, TTF_Font *font, SDL_Event *events,  int *success);//DONE
+int startQuiz( activityData *quiz , SDL_Renderer *render , TTF_Font *font , SDL_Event *events ,  int *success );//DONE
+
 //there was askQuestion here but it seemed unneccessary, so Its been folded into startQuiz
-char computeGrade(activityData *quiz, int noCorrect);//DONE
-void displayActivityResults(activityData *quiz, char *resultsToDisplay, SDL_Renderer *render, TTF_Font *font);//DONE
+char computeGrade( activityData *quiz , int noCorrect );//DONE
+
+void displayActivityResults( activityData *quiz , char *resultsToDisplay , SDL_Renderer *render , TTF_Font *font );//DONE
